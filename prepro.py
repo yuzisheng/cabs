b030c58fd4de78de60af45a9417addb2e7e44610
yuzisheng <yuzisheng3@jd.com>
2020-06-24 19:46:14 +0800

    heat map

--- prepro.py
+++ prepro.py
@@ -1,6 +1,11 @@
 import os
+import numpy as np
 import pandas as pd
+import seaborn as sns
 from datetime import datetime
+import matplotlib.pyplot as plt
+from common.mbr import MBR
+from common.grid import Grid
 
 
 def merge_to_one(root_path):
@@ -19,6 +24,36 @@ def merge_to_one(root_path):
     trajs[["oid", "lat", "lng", "time", "occ"]].to_csv("./data/trajs.csv", sep=",", index=False)
 
 
+def find_bbox(file_path):
+    trajs = pd.read_csv(file_path)
+    # lat:(32.8697, 50.30546) lng:(-127.08143, -115.56218)
+    print("lat: ({}, {})\nlng: ({}, {})".format(trajs["lat"].min(), trajs["lat"].max(),
+                                                trajs["lng"].min(), trajs["lng"].max()))
+
+
+def heat_map(file_path, grid):
+    res = np.zeros((grid.row_num, grid.col_num), dtype=np.int32)
+    data = pd.read_csv(file_path)
+    print(data.head())
+    for i in range(len(data)):
+        lat = data["lat"][i]
+        lng = data["lng"][i]
+        try:
+            row_idx, col_idx = grid.get_matrix_idx(lat, lng)
+            res[row_idx, col_idx] += 1
+        except IndexError:
+            # print('({0}, {1}) is out of mbr, skip'.format(lat, lng))
+            continue
+    sns.set()
+    sns.heatmap(res, square=True)
+    plt.show()
+
+
 if __name__ == '__main__':
-    merge_to_one("./data/taxi-sf")
+    # merge_to_one("./data/taxi-sf")
+    # find_bbox("./data/trajs.csv")
+    san_mbr = MBR(37.474, 122.637, 38.025, -121.926)  # 60km * 60km
+    san_grid = Grid(san_mbr, 600, 600)
+    heat_map("./data/trajs.csv", san_grid)
+
     print("ok")
